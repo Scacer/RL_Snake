c278bcd86cf9e7a883cb6b458be91adeae794c62
Scacer <60669273+Scacer@users.noreply.github.com>
2022-02-01 19:14:24 +0000

    RL Preperation
    The AI_snake_game.py file was renamed to game and a new file, agent.py, was created

--- game.py
+++ game.py
@@ -80,7 +80,7 @@ class AISnakeGame:
         #3 check if game over
         reward = 0
         game_over = False
-        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
             reward = -10
             return reward, game_over, self.score
@@ -102,7 +102,7 @@ class AISnakeGame:
         game_over = False
         return reward, game_over, self.score
 
-    def _is_collision(self, pt=None):
+    def is_collision(self, pt=None):
         if pt is None:
             pt = self.head
         #hits boundary
